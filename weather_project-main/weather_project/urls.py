d428b5e660fc14eac4c9d270804196ed82c01d37
Santanu kumar <8579santanu@gmail.com>
2026-04-12 09:50:22 +0530

    add this

--- weather_project-main/weather_project/urls.py
+++ weather_project-main/weather_project/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include, settings
+from django.urls import path, include
+from django.conf import settings
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', include('weather_app.urls')),
     path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'weather_app/images/favicon.png')),
 ]
-# In your urlpatterns:
-
