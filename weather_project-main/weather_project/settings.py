9e33370e7f14bf74516597dbeca8dc237226caaa
Santanu kumar <8579santanu@gmail.com>
2026-04-12 12:17:50 +0530

    add to change fab icon

--- weather_project-main/weather_project/settings.py
+++ weather_project-main/weather_project/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 
@@ -42,6 +43,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # OpenWeatherMap API Key (replace with your own key)
